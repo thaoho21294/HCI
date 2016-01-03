42049eeed6734f05580393dd45d1eb9b99fb3d10
thaoho21294 <thaoho21294@gmail.com>
2016-01-03 19:10:02 +0700

    Add project Lich chieu phim

---
+++ cinema-web/web/js/book.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	 //jump home
+    $(".btnghe").click(function(){
+								
+	
+        //$(".btnghe").css("background-image","url(../images/gehover.png)");
+		
+    });
+});
---
+++ cinema-web/web/js/home.js
@@ -0,0 +1,25 @@
+/*$(window).load(function() {
+		  $("#flexiselDemo1 #flexiselDemo2").flexisel({
+				visibleItems: 4,
+				animationSpeed: 1000,
+				autoPlay: true,
+				autoPlaySpeed: 3000,    		
+				pauseOnHover: false,
+				enableResponsiveBreakpoints: true,
+				responsiveBreakpoints: { 
+					portrait: { 
+						changePoint:480,
+						visibleItems: 2
+					}, 
+					landscape: { 
+						changePoint:640,
+						visibleItems: 3
+					},
+					tablet: { 
+						changePoint:768,
+						visibleItems: 3
+					}
+				}
+			});
+			});
+*/
